ccbd5be77a713394baac76192481f67297d86d16
Hemin Rasolpour <hassan.rasoulpour1@gmail.com>
2025-06-01 15:00:04 +0330

    Added post detail and list to Blog app's views

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.filter(published=True).order_by('-created_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, published=True)
+    next_post = Post.objects.filter(id__gt=post.id, published=True).order_by('id').first()
+    previous_post = Post.objects.filter(id__lt=post.id, published=True).order_by('-id').first()
+
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'next_post': next_post,
+        'previous_post': previous_post,
+    })
