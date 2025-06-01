e7d5e37fa14d3476c2e032bd15db5620398a939c
Hemin Rasolpour <hassan.rasoulpour1@gmail.com>
2025-06-01 14:11:43 +0330

    Added Post model and admin integration to Blog app

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    published_date = models.DateTimeField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
