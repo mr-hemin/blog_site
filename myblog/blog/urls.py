ce0568c877f0911e560fb01582dfd2f6c04e6ffb
Hemin Rasolpour <hassan.rasoulpour1@gmail.com>
2025-06-01 15:01:17 +0330

    Added blog templates and urls. Including base, index,detail

---
+++ myblog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, post_detail
+
+
+urlpatterns = [
+    path('', index, name='blog_index'),
+    path('<slug:slug>/', post_detail, name='post_detail'),
+]
